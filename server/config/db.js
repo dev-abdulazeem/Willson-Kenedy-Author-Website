ce80dab1120d1d84b74618ea0a52658e10423dfd
Bamidele Azeem <lilmaya490@gmail.com>
2026-06-16 09:21:16 -0400

    fixed backend version

--- server/config/db.js
+++ server/config/db.js
@@ -2,15 +2,14 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('connect', () => {
-  console.log('Connected to PostgreSQL');
+  console.log('Connected to Neon PostgreSQL');
 });
 
 pool.on('error', (err) => {
